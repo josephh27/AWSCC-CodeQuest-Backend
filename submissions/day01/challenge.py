6174b34ce1190efa00cca4f2c5aa1466f5632e98
Buhain, Joseph Jason S <josephjasonsbuhain@iskolarngbayan.pup.edu.ph>
2023-11-20 23:34:06 +0800

    Finished the day01 challenge.

---
+++ submissions/day01/challenge.py
@@ -0,0 +1,7 @@
+my_name = "Joseph Jason S. Buhain"
+name_of_department = "Department of Software and Web Development"
+birthdate = "January 27, 2003"
+hobbies = "programming, gaming, and watching series"
+age = 21
+print(f"""Hello, I am {my_name}. I love {name_of_department} and I love being in the Backend even more!
+I am born {birthdate} and I am {age} years old. My hobbies are {hobbies}.""")
