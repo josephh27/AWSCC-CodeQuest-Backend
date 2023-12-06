f7ec9c37f510f88e45b1ce26c392de92c0c7c62c
Buhain, Joseph Jason S <josephjasonsbuhain@iskolarngbayan.pup.edu.ph>
2023-12-06 10:30:07 +0800

    Finished the day12 challenge.

---
+++ submissions/day12/challenge.py
@@ -0,0 +1,16 @@
+class Dog:
+    def __init__(self, name, breed, color, age):
+        self.__name = name
+        self.__breed = breed
+        self.__color = color
+        self.__age = age
+
+    def bark(self):
+        print("Woof Woof!")
+
+    def show_details(self):
+        print(f"Name: {self.__name}\nBreed: {self.__breed}\nColor: {self.__color}\nAge: {self.__age}")
+
+my_dog = Dog("Michico", "Dachshund", "Brown", 4)
+my_dog.bark()
+my_dog.show_details()
